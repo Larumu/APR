8fd933443de1d780926432b26992185c01dd1c9e
Laura Ruiz Muñoz <laura_r.m@hotmail.com>
2017-12-23 17:37:33 +0100

    Update ejercicio4.m

--- ejercicio4.m
+++ ejercicio4.m
@@ -11,20 +11,24 @@ c=[0.0001 0.001 0.01 0.1 1 10 100 1000 10000];
 [h x] = size(c);
 for i = 1:y
   for j = 1:x
+  
     param = ["-q -t " num2str(t(i)) " -c " num2str(c(j))]
     res = svmtrain(trlabels, tr, param);
     newlabels = svmpredict(tslabels, ts, res, '');
     aciertos = 0;
     [col x] = size(tslabels);
+    
     for k = 1:col
        if tslabels(k) == newlabels(k)
            aciertos = aciertos+1;
        endif
     endfor
+    
     error = (1 - (aciertos/col))*100;
-    trustrangeleft = error - (1.96 *sqrt(error*(1-error)/col));
-    trustrangeright =error + (1.96 *sqrt(error*(1-error)/col));
-    trustrange = [trustrangeleft,trustrangeright]  
+    left = error - (1.96 *sqrt(error*(1-error)/col));
+    right =error + (1.96 *sqrt(error*(1-error)/col));
+    trustrange = [left,right]  
+    
   endfor
 endfor
 
