eaade555d701105f4059620146326555161ac90c
Laura Ruiz Muñoz <laura_r.m@hotmail.com>
2017-12-26 12:32:08 +0100

    Update Ejercicio1Update.m

--- Ejercicio1Update.m
+++ Ejercicio1Update.m
@@ -1,45 +1,33 @@
-clear
-
-if (nargin~=5)
-	printf('Wrong number of parameters');
-	exit(1);
-endif
+function [error] = p3(datos,etiquetas,datosTest,etiquetasTest,gauss)
+warning off all
 
 %Datos de entrenamiento
-arglist=argv();
-arg_1=arglist{1};
-disp('Loading data 1...');
-tr_dat = load(arg_1,'-ascii');
-disp('Data 1 load complete.');
 
-arg_2=arglist{2};
-disp('Loading data 2...');
-trlabel = load(arg_2,'-ascii');
-disp('Data 2 load complete.');
+tr_dat = load(datos,'-ascii');
+
+trlabel = load(etiquetas,'-ascii');
 tr_data = zscore(tr_dat);
 [numVec dim] = size(tr_data);
 numClas = max(trlabel)
+etiqApr = trlabel + 1;
 
 %Datos de test
-arg_3 = arglist{3};
-disp('Loading data 3...');
-tr_dat_test = load(arg_3,'-ascii');
-disp('Data 3 load complete.');
 
-arg_4 = arglist{4};
-disp('Loading data 4...');
-tr_label_test = load(arg_4,'-ascii');
-disp('Data 4 load complete.');
+tr_dat_test = load(datosTest,'-ascii');
+
+tr_label_test = load(etiquetasTest,'-ascii');
 tr_data_test = zscore(tr_dat_test);
+etiqTest = tr_label_test + 1;
 
 %Número de gaussianas por mixtura
 gaussianas = arglist{5};
 
-numGaus = gaussianas;
 grafo = [0 1 1; 0 0 1;0 0 0];
 numNodos = length(grafo);
 tallaNodos = [numClas numGaus dim];
 nodosDiscretos = [1 2];
+seed = 0;
+rand('state',seed);
 redB = mk_bnet(grafo,tallaNodos,'discrete',nodosDiscretos);
 redB.CPD{1} = tabular_CPD(redB,1);
 redB.CPD{2} = tabular_CPD(redB,2);
@@ -50,7 +38,8 @@ tr_datos(numNodos,:)= num2cell(tr_data',1);
 tr_datos(1,:)=num2cell(etiqApr',1);
 motor = jtree_inf_engine(redB);
 maxIter = 16;
-[redB2,l1,motor2]=learn_params_em(motor,tr_datos,maxIter);
+epsilon = 1e-20;
+[redB2,l1,motor2]=learn_params_em(motor,tr_datos,maxIter,epsilon);
 
 p = zeros(length(tr_data_test),numClas);
 evidencia = cell(numNodos,1);
@@ -59,4 +48,16 @@ for i =1:length(tr_data_test)
 	[motor3, l1] = enter_evidence(motor2,evidencia);
 	m = marginal_nodes(motor3,1);
 	p(i,:) = m.T';
+end
+error = 0;
+for(i = 1:length(p)
+	[val,ind] =max(p(i,:));
+	if(ind ~= etiqTest(i)) error = error +1;
+	end
+end
+errorr = (error(length(p));
+left = (errorr - (1.96*sqrt(errorr*(1-errorr)/length(p))));
+rigth = (errorr + (1.96*sqrt(errorr*(1-errorr)/length(p))));
+range = [100*left,100*right]
+errorr = errorr*100;
 end
