3069cfd1e573d4f01607151ce3ab0c790b7de0b6
Laura Ruiz Muñoz <laura_r.m@hotmail.com>
2017-12-26 18:14:21 +0100

    Update separable.m

--- separable.m
+++ separable.m
@@ -1,41 +1,34 @@
 clear
-#primero hay que cambiar el nombre de los sep que les falta el set en el archivo
 load trSep.dat;
 load trSeplabels.dat;
 
-#EJERCICIO 1:
-#entrenamiento de un SVM lineal sin kernel 
+#1; Entrenamiento de un SVM lineal sin kernel 
 res = svmtrain(trSeplabels, trSep, '-t 0 -c 1000');
 
-#EJERCICIO 2:
-# a) multiplicadores de Lagrange
+# 2a: Multiplicadores de Lagrange
 mult = res.sv_coef;
-# b) indices de los vectores soporte
+# 2b: Vectores soporte
 indc = res.sv_indices;
-# vectores soporte columnas
 Vec_soport = trSep(indc,:)' ;
-# c) vector de pesos (sumatorio de multiplicar cada vector soporte por su alfa correspondiente)
+# 2c: vector de pesos
 O = Vec_soport*mult
-# vector soporte arbitrario
 soport = Vec_soport(:,1);
-# signo de su alfa asociado (clase +-1)
 signo = sign(mult(1));
 umbral = signo - (O'*soport);
-# d) margen optimo es igual a 2 / modulo vector de pesos
+# 2d: margen 
 margen = 2/(norm(O));
 
-#EJERCICIO 3:
-# frontera lineal de la recta de separación
-	recta=0:1:7; 
-	cent=-(O(1)/O(2))*recta-(umbral/O(2));
+# 3: Frontera lineal de la recta de separación
+recta=0:1:8; 
+cent=-(O(1)/O(2))*recta-(umbral/O(2));
   
-#EJERCICIO 4:
-	plot(
-	trSep(trSeplabels==1,1),trSep(trSeplabels==1,2),"s",
-	trSep(trSeplabels==2,1),trSep(trSeplabels==2,2),"o",
-  Vec_soport'(:,1),Vec_soport'(:,2),"+k",
-	recta,cent,"g")
-	axis([0,7,0,7])
-  title('cuadrados y circulos = clases    simbolo + = vectores soporte')
-  xlabel('Eje X')
-  ylabel('Eje Y')
+# 4: Gráfico de los resultados
+plot(
+trSep(trSeplabels==1,1),trSep(trSeplabels==1,2),"s",
+trSep(trSeplabels==2,1),trSep(trSeplabels==2,2),"o",
+Vec_soport'(:,1),Vec_soport'(:,2),"+k",recta,cent,"g")
+
+axis([0,8,0,8])
+title('cuadrados y circulos = clases    simbolo + = vectores soporte')
+xlabel('Eje X')
+ylabel('Eje Y')
